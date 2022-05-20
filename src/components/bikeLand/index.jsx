498d82c981f2336fd63144196fb1e3f12a235bae
JSilver-Legend <procaptain516@gmail.com>
2022-05-20 23:13:03 +0900

    set camera rotation angle limit and union some objects

--- src/components/bikeLand/index.jsx
+++ src/components/bikeLand/index.jsx
@@ -1,17 +1,58 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Html } from '@react-three/drei';
 import Model from '../model';
 import './style.css';
+import { Camera } from 'three';
 
 const BikeLand = () => {
+
+  const camera = useRef();
+
+  const cameraMove = (value) => {
+    console.log('camera info-->', camera);
+  }
+
   return (
     <Suspense fallback={null}>
-      <Canvas className='canvas' camera={{ position: [0, 1, 0], fov: 50, focus: [10, 0, 10], rotation: [1, 1, 1] }}>
+      <Canvas
+        className='canvas'
+        // ref={camera}
+        camera={{ position: [-2.5, 3, 4], fov: 45 }}
+        shadows
+      >
         <fog attach="fog" args={[0xffffff, 6, 20]} intensity={1} />
+        <Html
+          position={[-0.8, 0.6, 1.5]}
+        >
+          <div className="button" onClick={() => cameraMove('shop')}>
+            Shop
+          </div>
+        </Html>
+        <Html
+          position={[-2.2, 0.7, 0.2]}
+        >
+          <div className="button" >
+            GateWay
+          </div>
+        </Html>
+        <Html
+          position={[1, 0.7, -0.2]}
+        >
+          <div className="button" >
+            Bar
+          </div>
+        </Html>
+        <Html
+          position={[2, 0.8, 2.8]}
+        >
+          <div className="button" >
+            Hill
+          </div>
+        </Html>
         <Model />
         <ambientLight intensity={0.8} />
-        <OrbitControls autoRotate={false} minDistance={5} maxDistance={5} minPolarAngle={2 * Math.PI / 8} maxPolarAngle={3.5 * Math.PI / 8} />
+        <OrbitControls ref={camera} rotateSpeed={0.4} minDistance={5} maxDistance={5} minPolarAngle={2 * Math.PI / 8} maxPolarAngle={2.3 * Math.PI / 8} />
       </Canvas>
     </Suspense>
   )
--- src/components/model/index.jsx
+++ src/components/model/index.jsx
@@ -1,198 +1,383 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './style.css';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, useAnimations } from '@react-three/drei';
 
 const Model = () => {
 
   const { nodes, materials, animations } = useGLTF('assets/glb/city_ground.glb');
   console.log('nodes-->', nodes);
 
+  const group = useRef();
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    // actions['All Animations'].play();
+  }, [actions])
+
+
   return (
     <group>
-      <group position={[0, 0, 0]} scale={[1, 1, 1]} dispose={null} >
+      <group ref={group} position={[0, 0, 0]} scale={[1, 1, 1]} dispose={null} >
         <mesh
           name='arc_gate'
           geometry={nodes.arc_gate.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_1'
           geometry={nodes.building_1.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_2'
           geometry={nodes.building_2.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_3'
           geometry={nodes.building_3.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_4'
           geometry={nodes.building_4.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_5'
           geometry={nodes.building_5.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='building_6'
           geometry={nodes.building_6.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_1'
           geometry={nodes.object_1.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_2'
           geometry={nodes.object_2.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_3'
           geometry={nodes.object_3.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_4'
           geometry={nodes.object_4.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_5'
           geometry={nodes.object_5.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name='object_6'
           geometry={nodes.object_6.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_1'
-          geometry={nodes.mountain_1.geometry}
+          name='object_7'
+          geometry={nodes.object_7.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_2'
-          geometry={nodes.mountain_2.geometry}
+          name='object_8'
+          geometry={nodes.object_8.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_3'
-          geometry={nodes.mountain_3.geometry}
+          name='object_9'
+          geometry={nodes.object_9.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_4'
-          geometry={nodes.mountain_4.geometry}
+          name='object_10'
+          geometry={nodes.object_10.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_5'
-          geometry={nodes.mountain_5.geometry}
+          name='object_11'
+          geometry={nodes.object_11.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
-          name='mountain_6'
-          geometry={nodes.mountain_6.geometry}
+          name='object_12'
+          geometry={nodes.object_12.geometry}
           material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name='mountain'
+          geometry={nodes.mountain.geometry}
+          material={materials.city_2}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_1"
           geometry={nodes.road_1.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_2"
           geometry={nodes.road_2.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_3"
           geometry={nodes.road_3.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_4"
           geometry={nodes.road_4.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_5"
           geometry={nodes.road_5.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_6"
           geometry={nodes.road_6.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_7"
           geometry={nodes.road_7.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_8"
           geometry={nodes.road_8.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_9"
           geometry={nodes.road_9.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_10"
           geometry={nodes.road_10.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_11"
           geometry={nodes.road_11.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_12"
           geometry={nodes.road_12.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_13"
           geometry={nodes.road_13.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_14"
           geometry={nodes.road_14.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_15"
           geometry={nodes.road_15.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_16"
           geometry={nodes.road_16.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
         <mesh
           name="road_17"
           geometry={nodes.road_17.geometry}
           material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_18"
+          geometry={nodes.road_18.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_19"
+          geometry={nodes.road_19.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_20"
+          geometry={nodes.road_20.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_21"
+          geometry={nodes.road_21.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_22"
+          geometry={nodes.road_22.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_23"
+          geometry={nodes.road_23.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_24"
+          geometry={nodes.road_24.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_25"
+          geometry={nodes.road_25.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_26"
+          geometry={nodes.road_26.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_27"
+          geometry={nodes.road_27.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_28"
+          geometry={nodes.road_28.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_29"
+          geometry={nodes.road_29.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_30"
+          geometry={nodes.road_30.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
+        />
+        <mesh
+          name="road_31"
+          geometry={nodes.road_31.geometry}
+          material={materials.city_1}
+          castShadow={true}
+          receiveShadow={true}
         />
       </group>
       <group>
-        <pointLight intensity={0.4} position={[100, 50, 100]} />
+        <pointLight intensity={0.6} position={[100, 50, 100]} castShadow={true} />
       </group>
     </group>
   );
